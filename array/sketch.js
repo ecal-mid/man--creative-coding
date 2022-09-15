e6063f93d09b3e54af9014ac73b4b33f78f69877
Sébastien Matos <matos.sebastien@gmail.com>
2022-09-15 10:54:26 +0200

    Add random index exercise

--- array/sketch.js
+++ array/sketch.js
@@ -2,39 +2,72 @@
 
 let diameter = 20;
 let gap = 10;
-let columns = 30;
-let rows = 10;
+let columns = 50;
+let rows = 30;
 let margin = 10;
 
 let colorRandom = []
+let randomDiameters = []
+let offsets = []
+
+let props = [] // [{ color, diameter, offset }, { color, diameter, offset }, ...]
 
 function setup() {
   createCanvas(0, 0)
   windowResized()
 
+
+  // console.log(propsOfOneCircle);
+
   let numberOfCircles = columns * rows
   for (let i = 0; i < numberOfCircles; i++) {
-    colorRandom[i] = color(random(255), random(255), random(255))
+
+    //* uncondensed version
+
+    // create different props
+    let colorRandom = color(random(255), random(255), random(255))
+    let diameter = random(10, 12)
+    let offset = random(10, 12)
+
+    // group them in an object, just like a folder
+    const obj = {
+      color: colorRandom,
+      diameter: diameter,
+      offset: offset,
+    }
+
+    // put the object on a specific slot in the array
+    props[i] = null
+
+    //* condensed version
+
+    // props[i] = {
+    //   color: color(random(255), random(255), random(255)),
+    //   diameter: random(10, 200),
+    //   offset: random(10, 200),
+    // }
+  }
+
+  let firstElem = props[0]
+  if (firstElem !== null) {
+    //* uncondensed
+    // console.log("firstElem of props ", firstElem);
+    // console.log("diameter of firstElem ", firstElem.diameter);
+
+    //* condensed
+    console.log(props[0].diameter);
+
   }
 
 }
 
-function mousePressed() {
-  colorRandom.push([0, 'lol', 'yes'])
-  // colorRandom[colorRandom.length] = [0, 'lol', 'yes']
-}
-
 function draw() {
-
-
 
   background('black')
 
   let offset = (margin + diameter / 2)
-  let color1 = color('red');
-  let color2 = color('green');
 
-  gap = map(mouseX, 0, width, 10, 20)
+  // gap = map(mouseX, 0, width, 10, 20)
 
   let index = 0
 
@@ -44,14 +77,13 @@ function draw() {
     // 
     for (let col = 0; col < columns; col++) {
 
-      let amount = map(col, 0, columns - 1, 0, 1);
-      let mixedColor = lerpColor(color1, color2, amount);
+      let obj = props[index]
 
-      mixedColor = colorRandom[index]
-      let x = col * (diameter + gap) + offset
-      fill(mixedColor)
-
-      ellipse(x, y, diameter)
+      if (obj !== null) {
+        let x = col * (diameter + gap) + offset
+        fill(obj.color)
+        ellipse(x, y + obj.offset, obj.diameter)
+      }
 
       index++;
     }
@@ -61,4 +93,20 @@ function draw() {
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight)
+}
+
+function mousePressed() {
+
+  let colorRandom = color(random(255), random(255), random(255))
+  let diameter = random(10, 10) * 20
+  let offset = random(10, 10)
+
+  //! complete this line
+  let randomIndex = 0
+
+  props[randomIndex] = {
+    color: colorRandom,
+    diameter: diameter,
+    offset: offset,
+  }
 }
